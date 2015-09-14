ada4b80c7229b682020ef4ed72462fe0458a3c2e
Bruno Passos <bruno.passos@booking.com>
2015-09-14 22:57:34 +0200

    Add spinner before dribble shots load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
-$(document).ready(function getDribbbleShots() {  
+$(document).ready(function () {  
+    
     setTimeout(function() {
         $('.profile img').fadeIn( "500" );
-    }, 500);    
-    $.jribbble.getShotsByPlayerId('brunopassos', function (playerShots) {    
-        var html = [];   
-        $.each(playerShots.shots, function (i, shot) {   
-            html.push('<div class="shot"><a href="' + shot.url + '" target="_blank">');    
-            html.push('<img class="shot-image" src="' + shot.image_url + '" ');    
-            html.push('alt="' + shot.title + '"></a></div>');    
-        });    
-        $('.dribbble-feed').html(html.join(''));   
-    },     
-    {    
-      page: 1,     
-      per_page: 15   
-    });    
+        
+        function showDribbbleShots() {
+            $.jribbble.getShotsByPlayerId('brunopassos', function (playerShots) {    
+                var html = [];   
+                $.each(playerShots.shots, function (i, shot) {   
+                    html.push('<div class="shot"><a href="' + shot.url + '" target="_blank">');    
+                    html.push('<img class="shot-image" src="' + shot.image_url + '" ');    
+                    html.push('alt="' + shot.title + '"></a></div>');    
+                });    
+                $('.dribbble-feed').html(html.join(''));   
+            },     
+            {    
+              page: 1,     
+              per_page: 15   
+            });
+        }
+        showDribbbleShots();
+        function hideSpinner() {
+            $('.spinner').hide();
+        }
+        hideSpinner();
+            
+    }, 500);
 });
 
