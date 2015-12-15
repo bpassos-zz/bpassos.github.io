e2c75628ec2c1c59eba8659a1bd17f30bb1563c2
Bruno Passos <bruno.passos@booking.com>
2015-12-15 21:58:50 +0100

    Removing Dribbble shots and tidying up unsused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,6 @@ $(document).ready(function () {
     setTimeout(function() {
         $('.profile img').fadeIn( "500" );
         
-        function showDribbbleShots() {
-            $.jribbble.getShotsByPlayerId('brunopassos', function (playerShots) {    
-                var html = [];   
-                $.each(playerShots.shots, function (i, shot) {   
-                    html.push('<div class="shot"><a href="' + shot.url + '" target="_blank">');    
-                    html.push('<img class="shot-image" src="' + shot.image_url + '" ');    
-                    html.push('alt="' + shot.title + '"></a></div>');    
-                });    
-                $('.dribbble-feed').html(html.join(''));   
-            },     
-            {    
-              page: 1,     
-              per_page: 15   
-            });
-        }
-        showDribbbleShots();
         function hideSpinner() {
             $('.spinner').hide();
         }
